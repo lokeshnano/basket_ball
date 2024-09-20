a0e009ebb29a19fb2b0ee565d247eaed0d76bf7e
lokesh <lokeshnano@gmail.com>
2024-09-21 02:44:08 +0530

    code

---
+++ index.js
@@ -0,0 +1,18 @@
+let initscore = document.getElementById("score")
+
+let count_ = 0
+function inc()
+{
+    count_ += 1
+    initscore.innerText = count_
+    console.log("clicked")
+}
+let initscore2 = document.getElementById("score2")
+
+let count_2 = 0
+function inc2()
+{
+    count_2 += 1
+    initscore2.innerText = count_2
+    console.log("clicked")
+}
